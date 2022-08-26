d5af17f0cf9fbf467c384678f8a2733d57250898
Eduardo Augusto <eduardo.evangelista2002@gmail.com>
2022-08-26 20:42:43 -0300

    feito_sem_mtos_widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Start());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Start extends StatelessWidget {
+  const Start({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // Radio byte Jr
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(22),
+                  margin: EdgeInsets.all(22),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(27),
+                    color: Color(0xFF3E3E3E),
+                  ),
+                  child: Text('Radio Byte Jr.',
+                      style: TextStyle(
+                        color: Color(0xFFFEB388),
+                        fontSize: 22,
+                      )),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+            // Imagem
+            Container(
+              padding: EdgeInsets.all(22),
+              width: size.width * 0.72,
+              height: size.width * 0.72,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFFABE2EA),
+              ),
+              child: Center(
+                  child: Text('IMAGEM',
+                      style: GoogleFonts.lato(
+                          fontSize: 48, fontWeight: FontWeight.w300))),
+            ),
+
+            // Numero, nome e Slider e botoes para mudar de faixa
+            SizedBox(width: size.width * 0.9, child: SliderCustomize()),
+
+            Spacer(),
+
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 12, bottom: 12, left: 20, right: 20),
+              margin: const EdgeInsets.all(30.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF3E3E3E),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: SizedBox(
+                height: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/images/byte.png',
+                      fit: BoxFit.fitHeight,
+                    ),
+                    Text('Projeto trainee 2022/2',
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Color(0xFFFEB388),
+                        )),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SliderCustomize extends StatefulWidget {
+  const SliderCustomize({Key? key}) : super(key: key);
+
+  @override
+  State<SliderCustomize> createState() => _SliderCustomizeState();
+}
+
+class _SliderCustomizeState extends State<SliderCustomize> {
+  void move_right() {
+    setState(() {
+      slider_value += 0.1;
+    });
+  }
+
+  void move_left() {
+    setState(() {
+      slider_value -= 0.1;
+    });
+  }
+
+  double slider_value = 90.2;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Nome e numero
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(slider_value.toStringAsFixed(1),
+                  style: GoogleFonts.lato(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                  )),
+              Text('Radio Oceano',
+                  style: GoogleFonts.lato(
+                      fontSize: 32, fontWeight: FontWeight.w700)),
+            ],
+          ),
+        ),
+        // Slider
+        SliderTheme(
+          data: SliderTheme.of(context).copyWith(
+            thumbColor: Color(0xFFFEB388),
+            activeTrackColor: Color(0xFF3E3E3E),
+            inactiveTrackColor: Color(0xFF3E3E3E),
+            overlayShape: RoundSliderOverlayShape(overlayRadius: 0),
+          ),
+          child: Slider(
+            value: slider_value,
+            min: 80,
+            max: 120,
+            onChanged: (double valor) {
+              setState(() {
+                slider_value = valor;
+              });
+            },
+          ),
+        ),
+
+        // Botao para esquerda e Texto FM e Botao para direita
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: move_left,
+              child: Icon(
+                Icons.keyboard_double_arrow_left,
+                color: Color(0xFF3E3E3E),
+              ),
+            ),
+            Text("FM",
+                style:
+                    GoogleFonts.lato(fontSize: 32, color: Color(0xFFFEB388))),
+            TextButton(
+              onPressed: move_right,
+              child: Icon(
+                Icons.keyboard_double_arrow_right,
+                color: Color(0xFF3E3E3E),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
